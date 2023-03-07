7393752fa1e6315c6a80b239e59398164b4c6852
Topher <TopherBirth@Gmail.com>
2023-03-07 15:50:16 -0600

    backup

--- src/App.js
+++ src/App.js
@@ -3,26 +3,69 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      token: '',
+      settings: []
+    }
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React Boi!
-          </a>
-        </header>
+      <div>
+        <table className="WHatever">
+          <thead>
+            <th>ID</th>
+            <th>Name</th>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
       </div>
     );
   }
+
+  componentWillMount() {
+    fetch('http://localhost:44881/misc/authenticate', authPayload)
+    .then(function(res) {
+      return res.json();
+     })
+    .then(data => {
+      let token = data.token;
+      this.setState({token: token});
+
+      var gpPayload = {  
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + this.state.token
+        }
+      }
+      
+      fetch('http://localhost:44881/settings/1', gpPayload)
+    .then(function(res) {
+      return res.json();
+     })
+    .then(data => {
+      let settings = data.results.map;
+      this.setState({settings: settings});
+     })
+    })
+  }
+}
+
+
+
+var authPayload = {  
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: '{"username":"chrisbirth@gmail.com","password":"testing12345"}'
 }
 
 export default App;
